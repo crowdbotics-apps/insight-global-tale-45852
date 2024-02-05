f5dd0857c237a67510754093b0b27254202e5ca4
Crowdbotics <team@crowdbotics.com>
2024-02-05 04:11:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "insight-global-tale-45852",
+  url: "https://insight-global-tale-45852.botics.co",
+  api: "https://insight-global-tale-45852.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
